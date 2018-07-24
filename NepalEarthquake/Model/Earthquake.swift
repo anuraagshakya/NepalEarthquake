b34b1df43a253b4c08c127311585a1c2e3355356
Anuraag Shakya <anuraag.shakya@gmail.com>
2018-07-24 18:02:08 +0200

    Create Earthquake data model

---
+++ NepalEarthquake/Model/Earthquake.swift
@@ -0,0 +1,26 @@
+//
+//  Earthquake.swift
+//  NepalEarthquake
+//
+//  Created by Anuraag Shakya on 24.07.18.
+//  Copyright © 2018 Bhunte. All rights reserved.
+//
+
+import Foundation
+
+// Model for Earthquake data derived from GeoJSON format defined by the USGS
+// here: https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+
+struct Earthquake {
+    var place: String
+    var time: UInt64
+    var mag: Float
+    var urlString: String
+    var location: Coordinates
+}
+
+struct Coordinates {
+    var longitude: Float
+    var latitude: Float
+    var depth: Float
+}
