f6f3e8a68a3eaa20381f2372a6e985a689d6d7e3
Anuraag Shakya <anuraag.shakya@sumup.com>
2020-04-20 19:17:53 +0200

    Decode JSON using codable

--- NepalEarthquake/Model/Earthquake.swift
+++ NepalEarthquake/Model/Earthquake.swift
@@ -11,16 +11,53 @@ import Foundation
 // Model for Earthquake data derived from GeoJSON format defined by the USGS
 // here: https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
 
-struct Earthquake {
+struct Earthquake: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case features
+    }
+
+    enum FeaturesCodingKeys: String, CodingKey {
+        case properties
+        case geometry
+    }
+
+    enum PropertiesCodingKeys: String, CodingKey, Encodable {
+        case title
+        case place
+        case time
+        case mag
+        case urlString
+    }
+
+    enum GeometryCodingKeys: String, CodingKey {
+        case coordinates
+    }
+
     var title: String
     var place: String
     var time: UInt64
     var mag: Float
     var urlString: String
     var location: Coordinates
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let features = try container.nestedContainer(keyedBy: FeaturesCodingKeys.self, forKey: .features)
+
+        let properties = try features.nestedContainer(keyedBy: PropertiesCodingKeys.self, forKey: .properties)
+        let geometry = try features.nestedContainer(keyedBy: GeometryCodingKeys.self, forKey: .geometry)
+
+        title = try properties.decode(String.self, forKey: .title)
+        place = try properties.decode(String.self, forKey: .place)
+        time = try properties.decode(UInt64.self, forKey: .time)
+        mag = try properties.decode(Float.self, forKey: .mag)
+        urlString = try properties.decode(String.self, forKey: .urlString)
+        location = try geometry.decode(Coordinates.self, forKey: .coordinates)
+    }
 }
 
-struct Coordinates {
+struct Coordinates: Codable {
     var longitude: Float
     var latitude: Float
     var depth: Float
--- NepalEarthquake/View Model/EarthquakeListViewModel.swift
+++ NepalEarthquake/View Model/EarthquakeListViewModel.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import SwiftyJSON
 
 class EarthquakeListViewModel {
     var dataSource: EarthquakeListDataSource
@@ -31,14 +30,7 @@ class EarthquakeListViewModel {
         if let url = URL(string: urlString) {
             DispatchQueue.global(qos: .userInitiated).async { [unowned self] in
                 if let data = try? String(contentsOf: url) {
-                    let json = JSON(parseJSON: data)
-                    
-                    if json["metadata"]["status"].intValue == 200  {
-                        self.parse(json: json)
-                        return
-                    } else {
-                        print("JSON status not OK")
-                    }
+                    self.parse(data: data.data(using: .utf8)!)
                 } else {
                     print("Could not read contents of file")
                 }
@@ -48,23 +40,15 @@ class EarthquakeListViewModel {
         }
     }
     
-    private func parse(json: JSON) {
-        var data = [Earthquake]()
-        for dataPoint in json["features"].arrayValue {
-            let title = dataPoint["properties"]["title"].stringValue
-            let place = dataPoint["properties"]["place"].stringValue
-            let time = dataPoint["properties"]["time"].uInt64Value
-            let mag = dataPoint["properties"]["mag"].floatValue
-            let urlString = dataPoint["properties"]["url"].stringValue
-            let coordinatesArray = dataPoint["geometry"]["coordinates"].arrayValue
-            let coordinates = Coordinates(
-                longitude: coordinatesArray[0].floatValue,
-                latitude: coordinatesArray[1].floatValue,
-                depth: coordinatesArray[2].floatValue)
-            
-            let earthquake = Earthquake(title: title, place: place, time: time, mag: mag, urlString: urlString, location: coordinates)
-            data.append(earthquake)
+    private func parse(data: Data) {
+        let decoder = JSONDecoder()
+        var earthQuakes = [Earthquake]()
+        do {
+            earthQuakes = try decoder.decode([Earthquake].self, from: data)
+        } catch {
+            assertionFailure("Could not decode JSON")
         }
-        self.dataSource.data = data
+
+        self.dataSource.data = earthQuakes
     }
 }
